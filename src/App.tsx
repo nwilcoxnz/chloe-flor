c882b953e4022b7a22895ef9f7fdc3ab8c5932a4
Nicole Wilcox <nwilcoxnz@gmail.com>
2022-10-24 22:50:03 +1300

    Changing to full height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Logo from "./assets/logo.png";
 const App = () => {
   return (
     <div className="bg-gradient-to-r from-teal-300 to-lime-300 h-screen">
-      <div className="flex items-center justify-center h-screen flex-col">
+      <div className="flex items-center justify-center h-full flex-col">
         <img src={Logo} className="h-16 sm:h-32" alt="Chloe Flor Logo" />
         <div>Coming soon</div>
       </div>
