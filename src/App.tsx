bfac8edc9167279beb2a629a165e089dd1cfe142
Nicole Wilcox <nwilcoxnz@gmail.com>
2022-10-24 22:10:37 +1300

    Adding alt text

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <div className="bg-gradient-to-r from-teal-300 to-lime-300 h-screen">
       <div className="flex items-center justify-center h-screen flex-col">
-        <img src={Logo} className="h-16 sm:h-32" />
-        <div className="">Coming soon</div>
+        <img src={Logo} className="h-16 sm:h-32" alt="Chloe Flor Logo" />
+        <div>Coming soon</div>
       </div>
     </div>
   );
