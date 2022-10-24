f7b514acae73de354f33f4660e9cde0e9eb8877a
Nicole Wilcox <nwilcoxnz@gmail.com>
2022-10-24 21:43:34 +1300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Logo from "./assets/logo.png";
+
+const App = () => {
+  return (
+    <div className="bg-gradient-to-r from-teal-300 to-lime-300 h-screen">
+      <div className="flex items-center justify-center h-screen flex-col">
+        <img src={Logo} className="h-16 sm:h-32" />
+        <div className="">Coming soon</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
