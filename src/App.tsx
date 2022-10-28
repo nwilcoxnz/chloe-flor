4f0531c19e8cf8c8e2e931386d7478618ecb6c02
Nicole Wilcox <nwilcoxnz@gmail.com>
2022-10-28 22:30:11 +1300

    Cleaning up rename and adding favicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="bg-gradient-to-r from-teal-300 to-lime-300 h-screen">
       <div className="flex items-center justify-center h-full flex-col">
-        <img src={Logo} className="h-16 sm:h-32" alt="Chloe Flor Logo" />
+        <img src={Logo} className="h-16 sm:h-32" alt="Daisy Flor Logo" />
         <div>Coming soon</div>
       </div>
     </div>
